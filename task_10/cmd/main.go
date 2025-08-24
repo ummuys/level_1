84e9a676068b79228f8f29f69c17924459638b4a
Ummuys <5percentofmilk@gmail.com>
2025-08-24 14:03:27 +0300

    task_10

---
+++ task_10/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func GroupTemp(temps []float64) map[int][]float64 {
+	group := make(map[int][]float64)
+	for _, temp := range temps {
+		key := int(temp) / 10 * 10
+		group[key] = append(group[key], temp)
+	}
+	return group
+}
+func main() {
+	temp := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+
+	fmt.Println(GroupTemp(temp))
+}
